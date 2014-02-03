d9fda8d22321bd32340aaf091c24ce152b610ad6
D3add3d <d3add3d@gmail.com>
2014-02-03 15:47:28 +0100

    Update BlockIDs.java

--- common/tk/d3add3d/cloaked/lib/BlockIDs.java
+++ common/tk/d3add3d/cloaked/lib/BlockIDs.java
@@ -1,6 +1,6 @@
 package tk.d3add3d.cloaked.lib;
 
 public class BlockIDs {
-	public static int BLOCK_one;
-	public static final int BLOCK_one_default = 1300;
+	public static int BLOCK_multiOne;
+	public static final int BLOCK_multiOne_default = 1300;
 }
